16683a9ae223c9da490619b03446ab880e039055
akira <domingo.munoz@gmail.com>
2017-02-01 13:06:39 +0100

    preparando commit

--- publish.sh
+++
@@ -1,6 +0,0 @@
-
-rsync -av ~/ionic-2/oikos/src/app/ ~/oikos-2/src/app/ionic2/app/
-rsync -av ~/ionic-2/oikos/src/pages/ ~/oikos-2/src/app/ionic2/pages/
-cd ~/oikos-2/
-git commit -a -m 'cambios en la app de ionic'
-git push
