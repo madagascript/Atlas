4c0d86ded1b4b4f9995713c4192cd7958e933b10
akira <domingo.munoz@gmail.com>
2017-02-03 14:05:05 +0100

    haciendo POST a Couchdb

--- src/app/oikos.service.ts
+++ src/app/oikos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable }     from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response, Headers, Request, RequestOptions, RequestMethod, URLSearchParams } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
@@ -11,20 +12,51 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class OikosService {
 	constructor (private http: Http) {}	
+
 	httpGetAsync(url, callback){
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback( JSON.parse(xmlHttp.responseText) );
-        }
-    xmlHttp.open("GET", url, true); // true for asynchronous 
-    xmlHttp.send(null);
+        let xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback( JSON.parse(xmlHttp.responseText) );
+            }
+        xmlHttp.open("GET", url, true); // true for asynchronous 
+        xmlHttp.send(null);
     }
+
+    httpPostAsync(url, obj, callback){
+        let xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                callback( JSON.parse(xmlHttp.responseText) );
+            }        
+        xmlHttp.open("POST", url, true); // true for asynchronous 
+        xmlHttp.setRequestHeader("Content-type", "application/json");        
+        xmlHttp.send();
+    }
+
 
   fileToJson(e: any, cb: any){         
     var reader = new FileReader();        
     reader.onload = () => cb(JSON.parse(reader.result));
     reader.readAsText(e.srcElement.files[0]);
     }
+
+  post(url: string, obj: any): Observable<any[]> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers, withCredentials: false });
+    return this.http.post(url, obj , options)
+                    .map( (res: Response) => {return res.json()})
+                    .catch(this.handleError)
+    }    
+
+  private handleError (error: Response | any) {    
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else { errMsg = error.message ? error.message : error.toString(); }
+    return Observable.throw(errMsg);
+    }      
 }
 
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { OikosService } from '../../app/oikos.service';
+
+
 const devlocal = true;
 const dataserver = 
   devlocal ? 
   'http://192.168.1.128:5984/ifcd0112/' : 
   'https://couchdb-52d18a.smileupps.com/ifcd0112/';
 const ejercicios = dataserver + '_design/app/_view/blog-date'
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html',
@@ -15,8 +18,24 @@ const ejercicios = dataserver + '_design/app/_view/blog-date'
 export class AboutPage implements OnInit {
 	lessons: any;    
   newlesson: any;
+  msg: any;
+  post: any = {
+    fecha: '2017-02-03',
+    titulo: 'prueba grabando post',
+    texto: 'en un lugar de la mancha',
+    bibliografia: []
+  }
+
   constructor(public navCtrl: NavController, public oS: OikosService) { }
   ngOnInit(){ }    
+
+  publicar(){
+    this.oS.post(dataserver, this.post).subscribe(
+      (data) => console.log(data),
+      (err) => console.log(err),
+      );
+    
+  }
 
   uploadDb(e){
     this.oS.fileToJson(e, data => this.newlesson = data)
