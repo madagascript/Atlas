c4cbc190b40886a4b20adb0aeef483e452e1b947
akira <domingo.munoz@gmail.com>
2018-07-22 15:14:43 +0200

    habilito CORS para todos los orígenes

--- server.js
+++ server.js
@@ -19,7 +19,11 @@ const {exec} = require('child_process');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/img', (request, response) => {
   let html = ''  
