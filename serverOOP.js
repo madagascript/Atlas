2f0062eff5ad2e323f634fdc586f3f882bf0ed57
akira <domingo.munoz@gmail.com>
2018-07-25 13:01:23 +0200

    ruta para descargar el server, para clase

--- serverOOP.js
+++ serverOOP.js
@@ -6,30 +6,30 @@ class App {
   }
   static showDocument(req, res){
     // los ObjectId de Mongo son string de 24 caracteres Hexadecimales, 
-    // si no mide 24, no hay que buscar porque petaría el server;
-    if ( request.params.id.length !== 24 ) { response.send({}) }
+    // si no mide 24, no hay que buscar porque petaría el server al crear el ObjectId();
+    if ( req.params.id.length !== 24 ) { res.send({}) }
     else {
-      mongoCli.db(request.params.db).collection(request.params.collection)
-      .findOne({ _id: new mongo.ObjectId(request.params.id) }, (err, results) => { response.send(results)} )  
+      mongoCli.db(req.params.db).collection(req.params.collection)
+      .findOne({ _id: new mongo.ObjectId(req.params.id) }, (err, results) => { res.send(results)} )  
     }
+  }
+  static server(req, res){
+    res.download('serverOOP.js');
   }
 }
 
 const express = require('express');
-const bodyParser = require('body-parser')
 const mongo = require('mongodb') 
 const app = express();
-const MongoClient = mongo.MongoClient;
 const url = process.argv[2]
 const port = process.argv[3] ? process.argv[3] : 3000
 var mongoCli = null;
 
-MongoClient.connect(url, function(err, client) {    
-  mongoCli = client
- });
+mongo.MongoClient.connect(url, (err, client) => mongoCli = client );
 
 app.get( '/:db/:collection', App.showCollection )
 app.get('/:db/:collection/:id', App.showDocument )
+app.get('/server', App.server )
 
 app.listen(port, function() {
   console.log(`server ok en puerto ${port}`)
