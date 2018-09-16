a3d584cbd1399ae13c192d8139fb8c0ae4568563
akira <domingo.munoz@gmail.com>
2018-09-16 14:41:47 +0200

    limpio public

--- serverOOP.js
+++ serverOOP.js
@@ -142,13 +142,12 @@ app.get('/server', App.server );
 app.get('/dbs', App.getDbs );
 app.get('/colls', App.getCollections ); // curl localhost:3000/colls?db=test
 app.get('/test', (req, res) => {
-  if ( isAdmin(req.query.credentials) ){ res.send('Ok!!') }
-  else { res.send('algo mal') }
   
 })
 
 app.listen(port, function() {
   console.log(`iniciado server en puerto ${port} a las ${new Date()}`);
 })
+
 
 
